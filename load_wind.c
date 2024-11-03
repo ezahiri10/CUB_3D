8e46bc5e61d5d01c056b70338ab1322972a40651
El Mustapha Zahiri <ezahiri@e1r6p4.1337.ma>
2024-11-03 22:47:26 +0100

    01

--- load_wind.c
+++ load_wind.c
@@ -6,7 +6,7 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 16:34:51 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/11/03 17:34:39 by ezahiri          ###   ########.fr       */
+/*   Updated: 2024/11/03 22:47:18 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,9 @@ void	init_mlx(t_data *data)
 	mlx_image_to_window (data->mlx, data->img, 0, 0);
 }
 
-void	frames(void *ptr)
-{
-	move_player(ptr);
-}
-
 void	load_wind(t_data *data)
 {
 	init_mlx (data);
-	mlx_loop_hook (data->mlx, frames, data);
+	mlx_loop_hook (data->mlx, move_player, data);
 	mlx_loop (data->mlx);
 }
