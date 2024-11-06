9ed0118cdcd7ffbd038d38f851abd4e71f354bad
El Mustapha Zahiri <ezahiri@e1r7p12.1337.ma>
2024-11-06 17:34:30 +0100

    dirction is fixed

--- drawed.c
+++ drawed.c
@@ -6,7 +6,7 @@
 /*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 16:40:36 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/11/06 17:29:51 by ezahiri          ###   ########.fr       */
+/*   Updated: 2024/11/06 17:33:45 by ezahiri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	get_texture(t_data *data, int i)
 	if (data->ray[i].h_or_v == 'v')
 	{
 		if (data->ray[i].left_right == 'l')
-			data->ray[i].j = 2;
-		if (data->ray[i].left_right == 'r')
 			data->ray[i].j = 3;
+		if (data->ray[i].left_right == 'r')
+			data->ray[i].j = 2;
 	}
 	if (data->ray[i].h_or_v == 'h')
 	{
