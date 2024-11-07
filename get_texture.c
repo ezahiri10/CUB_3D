c75f17b51903402bd4e3f63382d79189783acf1f
sel-hasn <fangyuansamach@gmail.com>
2024-11-07 12:07:31 +0100

    fix WE EA texture

--- get_texture.c
+++ get_texture.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_texture.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ezahiri <ezahiri@student.42.fr>            +#+  +:+       +#+        */
+/*   By: sel-hasn <sel-hasn@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/09 16:40:36 by ezahiri           #+#    #+#             */
-/*   Updated: 2024/11/04 10:50:59 by ezahiri          ###   ########.fr       */
+/*   Updated: 2024/11/07 12:06:35 by sel-hasn         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,9 @@ void	get_texture(t_data *data, int i)
 	if (data->ray[i].h_or_v == 'v')
 	{
 		if (data->ray[i].left_right == 'l')
-			data->ray[i].j = 2;
-		if (data->ray[i].left_right == 'r')
 			data->ray[i].j = 3;
+		if (data->ray[i].left_right == 'r')
+			data->ray[i].j = 2;
 	}
 	if (data->ray[i].h_or_v == 'h')
 	{
